186d9c76bfbd2ac24e572bfa6119f87360e4b505
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2022-04-30 02:24:48 +0000

    Initial commit

---
+++ src/main/java/tr/thelegend/wminereset/WMR.java
@@ -0,0 +1,17 @@
+package tr.thelegend.wminereset;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WMR extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+
+
+    }
+
+}
---
+++ src/main/java/tr/thelegend/wminereset/utils/Util.java
@@ -0,0 +1,11 @@
+package tr.thelegend.wminereset.utils;
+
+public class Util {
+
+    public void loadMines() {
+
+        
+
+    }
+
+}
